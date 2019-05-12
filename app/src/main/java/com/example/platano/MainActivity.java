26405bd87f8bc82ebaa6d17086ea1ed8d0298b9f
andy <andy.davier.martinez@gmail.com>
2019-05-11 22:01:17 -0400

    Capture image + select from gallery

--- app/src/main/java/com/example/platano/MainActivity.java
+++ app/src/main/java/com/example/platano/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.platano;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +13,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView userImage;
+    FloatingActionButton galleryButton;
+    FloatingActionButton cameraButton;
+    Uri imageUri;
+
+    private static final int PICK_IMAGE = 100;
+    private static final int OPEN_CAMERA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +33,46 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        userImage = (ImageView)findViewById(R.id.userImage);
+        galleryButton = (FloatingActionButton)findViewById(R.id.gallery);
+        cameraButton = (FloatingActionButton)findViewById(R.id.camera);
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               openGallery();
             }
         });
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    private void openCamera() {
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, OPEN_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            userImage.setImageURI(imageUri);
+        } else if (resultCode == RESULT_OK && requestCode == OPEN_CAMERA) {
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            userImage.setImageBitmap(bitmap);
+        }
+
     }
 
     @Override
